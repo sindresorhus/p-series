2b0b9e23a9f321d53af5dd816df45abbc16d5a23
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-05 23:29:24 +0200

    Refactor TypeScript definition to CommonJS compatible export

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,36 @@
-/**
- * Run promise-returning & async functions in series.
- *
- * @param tasks - Functions are expected to return a value. If a Promise is returned, it's awaited before continuing with the next task.
- * @returns A `Promise` that is fulfilled when all promises returned from calling the functions in `tasks` are fulfilled, or rejects if any of the promises reject. The fulfilled value is an `Array` of the fulfilled values.
- */
-export default function pSeries<ValueType>(
-	tasks: Iterable<() => Promise<ValueType> | ValueType>
-): Promise<ValueType[]>;
+declare const pSeries: {
+	/**
+	Run promise-returning & async functions in series.
+
+	@param tasks - Functions are expected to return a value. If a Promise is returned, it's awaited before continuing with the next task.
+	@returns A `Promise` that is fulfilled when all promises returned from calling the functions in `tasks` are fulfilled, or rejects if any of the promises reject. The fulfilled value is an `Array` of the fulfilled values.
+
+	@example
+	```
+	import pSeries = require('p-series');
+	import got = require('got');
+
+	(async () => {
+		const tasks = [
+			() => got('sindresorhus.com'),
+			() => checkSomething(),
+			() => doSomethingElse()
+		];
+
+		console.log(await pSeries(tasks));
+	})();
+	```
+	*/
+	<ValueType>(tasks: Iterable<() => Promise<ValueType> | ValueType>): Promise<
+		ValueType[]
+	>;
+
+	// TODO: Remove this for the next major release, refactor the whole definition to:
+	// declare function pSeries<ValueType>(
+	// 	tasks: Iterable<() => Promise<ValueType> | ValueType>
+	// ): Promise<ValueType[]>;
+	// export = pSeries;
+	default: typeof pSeries;
+};
+
+export = pSeries;
